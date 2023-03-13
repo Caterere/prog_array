abb71d9d0b5a88572d47e10a5bc19ae4c8566f33
Luiz Oro <dadoluizcat@gmail.com>
2023-03-13 08:22:41 -0300

    incluido o programa em js

---
+++ prog_array.js
@@ -0,0 +1,8 @@
+let res=document.getElementById("res")
+let nome_cidade = []
+
+function inserir(){
+     let cidade = document.getElementById("cidade").value
+     console.log(cidade)
+     nome_cidade.push(cidade)
+}
